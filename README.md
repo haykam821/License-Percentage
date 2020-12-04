0f842484d2a310857dde687d9a430a3961df383b
haykam821 <24855774+haykam821@users.noreply.github.com>
2020-12-04 11:51:58 -0500

    Filter libraries from the license percentage

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # License-Percentage
  Adds a license percentage to the title screen.
+ 
+ This mod uses the same logic to determine whether a mod is a license as [Mod Menu](https://github.com/TerraformersMC/ModMenu).
--- src/main/java/io/github/haykam821/licensepercentage/LicensePercentageUtils.java
+++ src/main/java/io/github/haykam821/licensepercentage/LicensePercentageUtils.java
@@ -2,8 +2,10 @@ package io.github.haykam821.licensepercentage;
 
 import java.text.DecimalFormat;
 import java.util.Locale;
+import java.util.regex.Pattern;
 
 import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.metadata.ModMetadata;
 import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
 
@@ -11,9 +13,30 @@ public final class LicensePercentageUtils {
 	private static final String COMPARISON_LICENSE = "arr";
 	private static final String TEXT_LICENSE = COMPARISON_LICENSE.toUpperCase(Locale.ROOT);
 	private static final DecimalFormat PERCENTAGE_FORMAT = new DecimalFormat("0.00");
+	private static final Pattern FABRIC_LIBRARY_PATTERN = Pattern.compile("^fabric-.*(-v\\d+)$");
+
+	private static boolean isLibrary(ModMetadata metadata) {
+		// Overrides
+		if (metadata.getId().equals("fabricloader")) return true;
+		if (metadata.getId().equals("fabric")) return true;
+		if (metadata.getId().equals("fabric-api-base")) return true;
+		if (metadata.getId().equals("fabric-renderer-indigo")) return true;
+		
+		// Patterns
+		if (metadata.getName().endsWith(" API")) return true;
+		if (FABRIC_LIBRARY_PATTERN.matcher(metadata.getId()).matches()) return true;
+
+		// Custom values
+		if (metadata.containsCustomValue("modmenu:api") && metadata.getCustomValue("modmenu:api").getAsBoolean()) return true;
+		if (metadata.containsCustomValue("fabric-loom:generated") && metadata.getCustomValue("fabric-loom:generated").getAsBoolean()) return true;
+
+		return false;
+	}
 
 	public static double getLicensePercentage() {
-		return FabricLoader.getInstance().getAllMods().stream().mapToDouble(container -> {
+		return FabricLoader.getInstance().getAllMods().stream().filter(container -> {
+			return !isLibrary(container.getMetadata());
+		}).mapToDouble(container -> {
 			return container.getMetadata().getLicense().stream().mapToInt(license -> {
 				return license.toLowerCase(Locale.ROOT).equals(COMPARISON_LICENSE) ? 1 : 0;
 			}).average().orElse(1);
